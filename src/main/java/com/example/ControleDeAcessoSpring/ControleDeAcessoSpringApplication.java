d7d851e242bf7e200906983d75ab1f3177b26896
PHCN <pedrohenrique.cn07@gmail.com>
2025-04-02 13:36:07 -0300

    Dia 02/04/2025 inicio do projeto Controle de acesso springboot em grupo

---
+++ src/main/java/com/example/ControleDeAcessoSpring/ControleDeAcessoSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.ControleDeAcessoSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleDeAcessoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleDeAcessoSpringApplication.class, args);
+	}
+
+}
