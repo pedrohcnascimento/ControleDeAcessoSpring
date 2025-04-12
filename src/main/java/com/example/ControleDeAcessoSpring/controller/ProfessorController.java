3aed3502d1d961987bea0ce643fa7a12902c312f
Arthur <arthurhenrrique1705@gmail.com>
2025-04-12 20:15:32 -0300

    Commit Configuração da API Rest

--- src/main/java/com/example/ControleDeAcessoSpring/controller/ProfessorController.java
+++ src/main/java/com/example/ControleDeAcessoSpring/controller/ProfessorController.java
@@ -1,7 +1,7 @@
 package com.example.ControleDeAcessoSpring.controller;
 
-import com.example.ControleDeAcessoSpring.model.entity.Coordenador;
-import com.example.ControleDeAcessoSpring.services.CoordenadorService;
+import com.example.ControleDeAcessoSpring.model.entity.Professor;
+import com.example.ControleDeAcessoSpring.services.ProfessorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -9,18 +9,18 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/coordenador")
-public class CoordenadorController {
+@RequestMapping("/professor")
+public class ProfessorController {
     @Autowired
-    private CoordenadorService service;
+    private ProfessorService service;
 
     @PostMapping
-    public Coordenador adicionarAluno(@RequestBody Coordenador obj) {
+    public Professor adicionarAluno(@RequestBody Professor obj) {
         return service.create(obj);
     }
 
     @GetMapping
-    public List<Coordenador> listarAlunos() {
+    public List<Professor> listarAlunos() {
         return service.getAll();
     }
 
--- src/main/java/com/example/ControleDeAcessoSpring/services/CoordenadorService.java
+++ src/main/java/com/example/ControleDeAcessoSpring/services/CoordenadorService.java
@@ -1,4 +1,47 @@
 package com.example.ControleDeAcessoSpring.services;
 
+import com.example.ControleDeAcessoSpring.model.entity.Coordenador;
+import com.example.ControleDeAcessoSpring.model.repositories.CoordenadorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class CoordenadorService {
+
+    @Autowired
+    private CoordenadorRepository repository;
+
+    public Coordenador create(Coordenador obj) {
+        return repository.save(obj);
+    }
+
+    public void delete(Long id) {
+        repository.deleteById(id);
+    }
+
+    public Coordenador getById(Long id) {
+        Optional<Coordenador> obj = repository.findById(id);
+        return obj.get();
+    }
+
+    public List<Coordenador> getAll() {
+        return repository.findAll();
+    }
+
+    public Coordenador update(Coordenador obj) {
+        Optional<Coordenador> newObj = repository.findById(obj.getId());
+        updateAluno(newObj, obj);
+        return repository.save(newObj.get());
+    }
+
+    private Coordenador updateAluno(Optional<Coordenador> newObj, Coordenador obj) {
+        newObj.get().setNome(obj.getNome());
+        newObj.get().setListaDeProfessores(obj.getListaDeProfessores());
+        newObj.get().setIdade(obj.getIdade());
+        return obj;
+    }
 }
+
