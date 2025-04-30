b1ef581c2f79ebd44846d9eda1bbbe9758b9c844
Arthur <arthurhenrrique1705@gmail.com>
2025-04-30 15:06:04 -0300

    Arrumando repositories

--- src/main/java/com/senai/ControleDeAcessoSpring/domain/repositories/JustificativaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.senai.ControleDeAcessoSpring.domain.repositories;
-
-import com.senai.ControleDeAcessoSpring.domain.entity.Justificativa;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface JustificativaRepository extends JpaRepository<Justificativa, Long> {
-}
--- src/main/java/com/senai/ControleDeAcessoSpring/domain/repositories/OcorrenciaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.senai.ControleDeAcessoSpring.domain.repositories;
-
-import com.senai.ControleDeAcessoSpring.domain.entity.Ocorrencia;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OcorrenciaRepository extends JpaRepository<Ocorrencia, Long> {
-}
