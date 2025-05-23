a4c3a27f5afdace11cbbc69763802ab40bb4fdef
Arthur <arthurhenrrique1705@gmail.com>
2025-05-23 16:53:15 -0300

    Revisado e aprovado

--- src/main/java/com/senai/ControleDeAcessoSpring/domain/entity/turma/SubTurma.java
+++ src/main/java/com/senai/ControleDeAcessoSpring/domain/entity/turma/SubTurma.java
@@ -20,9 +20,10 @@ public class SubTurma {
     private Boolean status;
 
     @ManyToOne
+    @JoinColumn(name = "turma_id")
     private Turma turma;
 
-    @ManyToMany(mappedBy = "subTurmas")
+    @ManyToMany(mappedBy = "subTurmas", cascade = CascadeType.ALL)
     private List<Aluno> alunos;
 
     @OneToOne(cascade = CascadeType.ALL)
--- src/main/java/com/senai/ControleDeAcessoSpring/domain/entity/usuarios/Professor.java
+++ src/main/java/com/senai/ControleDeAcessoSpring/domain/entity/usuarios/Professor.java
@@ -2,8 +2,12 @@ package com.senai.ControleDeAcessoSpring.domain.entity.usuarios;
 
 import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 
 @Entity
 @DiscriminatorValue("PROFESSOR")
+@NoArgsConstructor
+@AllArgsConstructor
 public class Professor extends Usuario {
 }
