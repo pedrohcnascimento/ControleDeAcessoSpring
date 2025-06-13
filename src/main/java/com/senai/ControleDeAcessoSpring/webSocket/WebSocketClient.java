3eaa634fd5ccfc9598b859925eeccf5bdd8bcd59
Arthur <arthurhenrrique1705@gmail.com>
2025-06-13 15:39:12 -0300

    Criando classe para Cliente webSocket

--- src/main/java/com/senai/ControleDeAcessoSpring/webSocket/WebSocketClient.java
+++ src/main/java/com/senai/ControleDeAcessoSpring/webSocket/WebSocketClient.java
@@ -1,5 +1,66 @@
 package com.senai.ControleDeAcessoSpring.webSocket;
 
+import jakarta.websocket.*;
+
+import java.net.URI;
+import java.util.Timer;
+import java.util.TimerTask;
+
+@ClientEndpoint
 public class WebSocketClient {
 
+    private static Session session;
+    private static Timer pingTimer;
+
+    @OnOpen
+    public void onOpen(Session s) {
+        session = s;
+        System.out.println("Conexão efetuada com sucesso!");
+    }
+
+    @OnMessage
+    public void onMessage (String msg) {
+        System.out.println("Mensagem recebida por WebSocket: " + msg);
+    }
+
+    @OnClose
+    public void onClose(Session s, CloseReason reason) {
+        System.out.println("Sessão encerrada, motivo: " + reason);
+    }
+
+    public static void conectar() {
+        try {
+            WebSocketContainer container = ContainerProvider.getWebSocketContainer();
+            URI uri = new URI("ws://localhost:8080/ws");
+            container.connectToServer(WebSocketClient.class, uri);
+
+            pingTimer = new Timer(true);
+            pingTimer.scheduleAtFixedRate(new TimerTask() {
+                @Override
+                public void run() {
+                    if (session != null) {
+                        try {
+                            session.getBasicRemote().sendText("ping");
+                        } catch (Exception ignored) {}
+                    }
+                };
+            }, 20_000, 20_000);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void desconectar() {
+        try {
+            if (pingTimer != null) {
+                pingTimer.cancel();
+            }
+            if (session != null && session.isOpen()) {
+                session.close();
+                System.out.println("Desconectado do WebSocket.");
+            }
+        } catch (Exception e) {
+            System.err.println("Erro ao fechar WebSocket: " + e.getMessage());
+        }
+    }
 }
