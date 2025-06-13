7af2cd091ddab25ef93b63c8f2f6849e33e4ccce
Lucas de Matos Pires <dematospireslucas@gmail.com>
2025-06-13 13:37:15 -0300

    teste

--- src/main/java/com/senai/ControleDeAcessoSpring/aplication/dto/AmbienteDTO.java
+++ src/main/java/com/senai/ControleDeAcessoSpring/aplication/dto/AmbienteDTO.java
@@ -6,50 +6,50 @@ import lombok.Data;
 @Data
 public class AmbienteDTO {
 
-    private Long id;
-    private String nome;
-    private String descricao;
-    private boolean ativo = true;
+private Long id;
+private String nome;
+private String descricao;
+private boolean ativo = true;
 
-    public Ambiente toEntity() {
-        Ambiente ambiente = new Ambiente();
-        ambiente.setId(this.id);
-        ambiente.setNome(this.nome);
-        ambiente.setDescricao(this.descricao);
-        ambiente.setAtivo(this.ativo);
-        return ambiente;
-    }
-
-    public static AmbienteDTO fromEntity(Ambiente ambiente) {
-        AmbienteDTO dto = new AmbienteDTO();
-        dto.setId(ambiente.getId());
-        dto.setNome(ambiente.getNome());
-        dto.setDescricao(ambiente.getDescricao());
-        dto.setAtivo(ambiente.isAtivo());
-        return dto;
-    }
-
-    public String getNome() {
-        return this.nome;
-    }
-
-    public String getDescricao() {
-        return this.descricao;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
-
-    public void setAtivo(boolean ativo) {
-        this.ativo = ativo;
-    }
+public Ambiente toEntity() {
+    Ambiente ambiente = new Ambiente();
+    ambiente.setId(this.id);
+    ambiente.setNome(this.nome);
+    ambiente.setDescricao(this.descricao);
+    ambiente.setAtivo(this.ativo);
+    return ambiente;
 }
+
+public static AmbienteDTO fromEntity(Ambiente ambiente) {
+    AmbienteDTO dto = new AmbienteDTO();
+    dto.setId(ambiente.getId());
+    dto.setNome(ambiente.getNome());
+    dto.setDescricao(ambiente.getDescricao());
+    dto.setAtivo(ambiente.isAtivo());
+    return dto;
+}
+
+public String getNome() {
+    return this.nome;
+}
+
+public String getDescricao() {
+    return this.descricao;
+}
+
+public void setId(Long id) {
+    this.id = id;
+}
+
+public void setNome(String nome) {
+    this.nome = nome;
+}
+
+public void setDescricao(String descricao) {
+    this.descricao = descricao;
+}
+
+public void setAtivo(boolean ativo) {
+    this.ativo = ativo;
+}
+} ;;
