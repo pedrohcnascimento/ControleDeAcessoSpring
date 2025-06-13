a1be34502e10e2f9d4a1ef96c27be009d20da583
arthur <arthurhenrrique1705@gmail.com>
2025-06-12 22:45:18 -0300

    Construindo metodos para definir semestre e aula do aluno e terminando de construir a ocorrencia.

--- src/main/java/com/senai/ControleDeAcessoSpring/aplication/service/usuarios/aluno/OcorrenciaService.java
+++ src/main/java/com/senai/ControleDeAcessoSpring/aplication/service/usuarios/aluno/OcorrenciaService.java
@@ -1,20 +1,30 @@
 package com.senai.ControleDeAcessoSpring.aplication.service.usuarios.aluno;
 
 import com.senai.ControleDeAcessoSpring.aplication.dto.usuarios.aluno.OcorrenciaDto;
+import com.senai.ControleDeAcessoSpring.domain.entity.turma.Semestre;
 import com.senai.ControleDeAcessoSpring.domain.entity.turma.SubTurma;
+import com.senai.ControleDeAcessoSpring.domain.entity.turma.horario.Aula;
+import com.senai.ControleDeAcessoSpring.domain.entity.turma.horario.AulaDoDia;
+import com.senai.ControleDeAcessoSpring.domain.entity.turma.horario.HorarioPadrao;
 import com.senai.ControleDeAcessoSpring.domain.entity.usuarios.Usuario;
 import com.senai.ControleDeAcessoSpring.domain.entity.usuarios.aluno.Aluno;
 import com.senai.ControleDeAcessoSpring.domain.entity.usuarios.aluno.Ocorrencia;
+import com.senai.ControleDeAcessoSpring.domain.enums.DiaDaSemana;
 import com.senai.ControleDeAcessoSpring.domain.enums.Periodo;
 import com.senai.ControleDeAcessoSpring.domain.enums.StatusDaOcorrencia;
+import com.senai.ControleDeAcessoSpring.domain.enums.TipoDeOcorrencia;
 import com.senai.ControleDeAcessoSpring.domain.repository.usuarios.aluno.OcorrenciaRepository;
 import com.senai.ControleDeAcessoSpring.domain.repository.usuarios.UsuarioRepository;
 import jakarta.transaction.Transactional;
+import jdk.dynalink.linker.ConversionComparator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -89,6 +99,27 @@ public class OcorrenciaService {
                 if (st != null) {
                     System.out.println("Subturma: " + st.getNome());
                     System.out.println("Periodo: " + st.getTurma().getPeriodo().name());
+
+                    Semestre semestre = definirSemestre(st);
+
+                    System.out.println("Semestre: " + semestre.getId());
+
+                    Aula aula = definirAula(semestre, st);
+
+                    System.out.println("Aula referida: " + aula.getUnidadeCurricular().getNome());
+                    System.out.println("Professor responsavel: " + aula.getProfessor().getNome());
+                    System.out.println("Sala: " + aula.getAmbiente().getNome());
+
+                    Ocorrencia novaOcorrencia = new Ocorrencia();
+
+                    novaOcorrencia.setAluno(aluno);
+                    novaOcorrencia.setAtivo(true);
+                    novaOcorrencia.setTipo(TipoDeOcorrencia.ATRASO);
+                    novaOcorrencia.setDataHoraCriacao(LocalDateTime.now());
+                    novaOcorrencia.setProfessorResponsavel(aula.getProfessor());
+                    novaOcorrencia.setUnidadeCurricular(aula.getUnidadeCurricular());
+
+                    ocorrenciaRepository.save(novaOcorrencia);
                 }
             }
         } else {
@@ -124,6 +155,55 @@ public class OcorrenciaService {
         }
 
         return subTurma;
+    }
+
+    private Semestre definirSemestre(SubTurma subTurma) {
+        Semestre semestre = null;
+
+        LocalDate dataInicial = subTurma.getTurma().getDataInicial();
+        LocalDate hoje = LocalDate.now();
+
+        long mesesPassados = ChronoUnit.MONTHS.between(dataInicial, hoje);
+
+        semestre = subTurma.getSemestres().get(((int) mesesPassados / 6));
+
+        return semestre;
+    }
+
+    private Aula definirAula(Semestre semestre, SubTurma subTurma) {
+        HorarioPadrao horario = semestre.getHorarioPadrao();
+        DiaDaSemana diaDaSemanaHoje = (
+                switch (LocalDate.now().getDayOfWeek()) {
+                    case MONDAY -> DiaDaSemana.SEGUNDA;
+                    case TUESDAY -> DiaDaSemana.TERCA;
+                    case WEDNESDAY -> DiaDaSemana.QUARTA;
+                    case THURSDAY -> DiaDaSemana.QUINTA;
+                    case FRIDAY -> DiaDaSemana.SEXTA;
+                    default -> DiaDaSemana.SEGUNDA;
+                }
+                );
+
+        AulaDoDia aulas = horario.getAulasDoDia().stream()
+                .filter(aulaDoDia -> aulaDoDia.getDiaDaSemana().equals(diaDaSemanaHoje))
+                .findFirst()
+                .orElse(null);
+
+        if (aulas != null) return null;
+
+        LocalTime agora = LocalTime.now();
+        LocalTime inicioDoDia = subTurma.getTurma().getHorarioEntrada();
+
+        long minutosPassados = (
+                (agora.getHour() * 60 + agora.getMinute()) +
+                (inicioDoDia.getHour() * 60 + agora.getMinute())
+        );
+
+        int aula = (int)
+                (minutosPassados /
+                        subTurma.getTurma().getCurso().getTipo().getMinutosPorAula())
+                + 1;
+
+        return aulas.getAulas().get(aula);
     }
 }
 
